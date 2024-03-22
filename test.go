28f5e85c4425c63f6de491068f55326dd4f36a30
john <johnbalvin1994@gmail.com>
2024-03-22 00:04:35 -0500

    fixing example

--- test.go
+++ test.go
@@ -12,23 +12,43 @@ func Test() {
 	test1()
 }
 func test1() {
-	input := search.InputData{
-		ZoomValue: 9,
-		Coordinates: search.CoordinatesInput{
-			Ne: search.CoordinatesValues{
-				Latitude: 43.389689973008714,
-				Longitud: -72.41662360750962,
-			},
-			Sw: search.CoordinatesValues{
-				Latitude: 40.07030481778054,
-				Longitud: -78.84362556063462,
-			},
+	coords := CoordinatesInput{
+		Ne: search.CoordinatesValues{
+			Latitude: 43.389689973008714,
+			Longitud: -72.41662360750962,
+		},
+		Sw: search.CoordinatesValues{
+			Latitude: 40.07030481778054,
+			Longitud: -78.84362556063462,
 		},
 	}
-	results, err := input.SearchAll(nil)
+	zomValue := 3
+	results, err := SearchAll(zomValue, coords, nil)
 	if err != nil {
 		log.Println("err: ", err)
 	}
 	f, _ := os.Create("./results.json")
 	json.NewEncoder(f).Encode(results)
 }
+
+/*
+func test2() {
+	propertyURL := ""
+	results, err := GetFromPropertyURL(propertyURL, nil)
+	if err != nil {
+		log.Println("err: ", err)
+	}
+	f, _ := os.Create("./results.json")
+	json.NewEncoder(f).Encode(results)
+}
+
+func test3() {
+	propertyURL := ""
+	results, err := GetFromPropertyID(propertyURL, nil)
+	if err != nil {
+		log.Println("err: ", err)
+	}
+	f, _ := os.Create("./results.json")
+	json.NewEncoder(f).Encode(results)
+}
+*/
